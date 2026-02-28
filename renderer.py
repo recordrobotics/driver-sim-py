70c11f977c682547a2be9aa54bdba1afee59d1f9
Compdog Inc. <compdog.inc@gmail.com>
2026-02-27 19:04:54 -0500

    Fix bug

--- renderer.py
+++ renderer.py
@@ -236,8 +236,7 @@ class RealtimeRenderer(mglw.WindowConfig):
         super().__init__(**kwargs)
         shader_dir = Path(__file__).resolve().parent / "shaders"
         shader_dir_str = str(shader_dir)
-        if shader_dir_str not in [str(path) for path in settings.PROGRAM_DIRS]:
-            settings.PROGRAM_DIRS.append(shader_dir_str)
+        settings.PROGRAM_DIRS = [shader_dir_str]
         self.assets = load_assets_config(ASSETS_ROOT)
         self.nt_client = NetworkTablesClient()
         imgui.create_context()
